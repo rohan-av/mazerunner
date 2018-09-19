4b9d99cd17c2bb5612c2b701123f437295f96f0b
Rohan Arya Varma <40168954+rohan-av@users.noreply.github.com>
2018-09-19 10:57:35 +0800

    Update testcase.cpp
    The code contains a test case, where the design and structure of the maze was coded manually into the Maze object. This code will be the foundation for a more general program where there are better and more flexible input options.

--- testcase.cpp
+++ testcase.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 #include <list>
-
+ 
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
